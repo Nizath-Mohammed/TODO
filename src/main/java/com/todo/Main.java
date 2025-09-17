95ad4eea883e021a85301bc117eef07886885322
Nizath-mohammed <Kit27.ad36@gmail.com>
2025-09-17 14:48:49 +0530

    new changes

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,20 +1,41 @@
 package com.todo;
-import com.todo.util.DatabaseConnection;
 
-import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.todo.gui.TodoAppGUI;
+import com.todo.util.DatabaseConnection;
 public class Main{
-    public static void main(String[] args){
-    DatabaseConnection db = new DatabaseConnection();
-    try{
-        Connection cn=db.getDBConnection();
-        System.out.println("database connected Successfully");
-    }
-    catch(SQLException e){
-        System.out.println("database connected Failure");
-
-    }
-
-
-}
+    public static void main(String[] args) {
+        DatabaseConnection db_Connection = new DatabaseConnection();
+        try{
+            Connection cn=db_Connection.getDBConnection();
+            System.out.println("databse connected successfully");
         }
+        catch(SQLException e){
+            System.out.print("The Database Connection failed");
+            System.exit(1);
+        }
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e){
+            System.err.println("Could not set look and feel"+e.getMessage());
+        }
+
+        SwingUtilities.invokeLater(
+                () -> {
+                    try{
+                        new TodoAppGUI().setVisible(true);
+                    }
+                    catch(Exception e){
+                        System.err.println("Error Starting Apllication "+e.getLocalizedMessage());
+                    }
+                }
+        );
+    }
+}
---
+++ src/main/java/com/todo/gui/TodoAppGUI.java
@@ -0,0 +1,98 @@
+package com.todo.gui;
+import com.todo.dao.TodoAppDAO;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class TodoAppGUI extends JFrame {
+    private TodoAppDAO tododao;
+    private DefaultTableModel tableModel;
+    private JTable todoTable;
+    private JTextField titleField;
+    private JTextArea descriptionArea;
+    private JCheckBox completedCheckBox;
+    private JButton addButton;
+    private JButton deleteButton;
+    private JButton updateButton;
+    private JButton refreshButton;
+    private JComboBox<String> fillterComboBox;
+
+    public TodoAppGUI() {
+        this.tododao= new TodoAppDAO();
+        initializeComponents();
+        setupLayout();
+    }
+
+    private void initializeComponents() {
+        setTitle("Todo Apllication");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+
+        String[] columnNames={"ID","Title","Description","Completed","Created At","Updated At"};
+        tableModel=new DefaultTableModel(columnNames,0){
+            @Override
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+        };
+
+        todoTable =new JTable(tableModel);
+        //
+        todoTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        todoTable.getSelectionModel().addListSelectionListener(
+                (e) -> {
+                    if(!e.getValueIsAdjusting()){
+//                  loadSelectedtodo();
+                    }
+                }
+        );
+
+        titleField =new JTextField(20);
+        descriptionArea= new JTextArea(3,20);
+        descriptionArea.setLineWrap(true);
+//      would wrap to the next line instead of going beyond the box.
+        descriptionArea.setWrapStyleWord(true);
+
+        completedCheckBox=new JCheckBox("completed");
+        addButton= new JButton("Add Todo");
+        updateButton=new JButton ("Update Todo");
+        deleteButton= new JButton("Delte Todo");
+        refreshButton= new JButton("Refresh Todo");
+
+        String[] fillterOptions={"All","Completed","Pending"};
+        fillterComboBox =new JComboBox<>(fillterOptions);
+        fillterComboBox.addActionListener(
+                (e)->{
+
+                }
+        );
+
+    }
+
+    private void setupLayout(){
+        setLayout(new BorderLayout());
+        JPanel inputPanel =new JPanel(new GridBagLayout());
+//        GridBagLayout that follows a table layout that follows row and col
+        GridBagConstraints gbc = new GridBagConstraints(); // we are getting this from the awt liberies (it tell about how element going to present in the Jpanel)
+        gbc.insets = new Insets(5,5,5,5);// this gbc.insets requiring an Insets class object ( so this was a reference variable of the Insets class object) |(this will tell the how much distance should be present between each element )
+        gbc.gridx = 0; // this will act as a x coordinates (col)
+        gbc.gridy = 0; // this will act as a y coordinates (x)
+        gbc.anchor= GridBagConstraints.WEST;
+        inputPanel.add(new JLabel("Title"),gbc); // mention the titleField->TextField we are placing inside a JPanel->inputPanel
+        gbc.gridx=1;
+        gbc.fill= GridBagConstraints.HORIZONTAL;
+        inputPanel.add(titleField,gbc);
+        gbc.gridx=0;
+        gbc.gridy=1;
+        inputPanel.add(new JLabel("Description"),gbc);
+        gbc.gridx=1;
+        inputPanel.add(descriptionArea,gbc);
+        add(inputPanel,BorderLayout.NORTH);
+//        gbc.gridx=1;
+//        gbc.gridy=2;
+//        inputPanel.add(new JLabel(""))
+
+    }
+}
